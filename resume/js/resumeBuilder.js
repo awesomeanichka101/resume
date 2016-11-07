3cfe0c3867aad07accfe6af45d1c7e3034143ed2
Anna Stein <gymnastics.level15@gmail.com>
2016-11-07 15:39:29 -0500

    Add projects to resume

--- resume/js/resumeBuilder.js
+++ resume/js/resumeBuilder.js
@@ -54,23 +54,61 @@ var work = {
     ]
 }
 
-// Working with for-in loops
-for (job in work.jobs) {
-	$("#workExperience").append(HTMLworkStart);
+function displayWork() {
+	for (job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
 
-	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-	var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
-	var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-	var formattedEverything = formattedEmployer + formattedTitle + formattedDates + formattedWorkLocation + formattedDescription;
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		var formattedEverything = formattedEmployer + formattedTitle + formattedDates + formattedWorkLocation + formattedDescription;
 
-	$(".work-entry:last").append(formattedEverything);
+		$(".work-entry:last").append(formattedEverything);
+	}
 }
 
-//JSON practice
+displayWork();
 
-var project = {};
+var projects = {
+	"projects" : [
+		{
+			"title" : "Future Limberlost Layers Website",
+			"dates" : "December 2016 - February 2017",
+			"description" : "This is the website for my dad's egg business, Limberlost Layers. Started from a WordPress theme and revised for accuracy."
+		},
+		{
+			"title" : "Another Future Project",
+			"dates" : "March 2017 - July 2017",
+			"description" : "Whatever future project I do goes here. I have no idea what it will be right now though."
+		}
+	]
+};
+projects.display = function() {
+	for (project in projects.projects) {
+		$('#projects').append(HTMLprojectStart);
+
+		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+		$(".project-entry:last").append(formattedTitle);
+
+		var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+		$(".project-entry:last").append(formattedDates);
+
+		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+		$(".project-entry:last").append(formattedDescription);
+
+		// if you now have images of your projects in var project, then uncomment everything after this and you will see them in resume
+		//if (projects.projects[project].images.length > 0) {
+		//	for (image in projects.projects[project].images) {
+		//		var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+		//		$(".project-entry:last").append(formattedImage);
+		//	}
+		//}
+	}
+}
+
+projects.display();
 
 var education = {
 	"schools": [
