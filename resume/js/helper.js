cd73640ba144985750dc617d4a68ee0de8433341
Anna Stein <gymnastics.level15@gmail.com>
2016-11-08 15:46:24 -0500

    Add Google Map to resume

--- resume/js/helper.js
+++ resume/js/helper.js
@@ -127,7 +127,7 @@ function initializeMap() {
     var locations = [];
 
     // adds the single location property from bio to the locations array
-    locations.push(bio.contacts.location);
+    locations.push(bio.location);
 
     // iterates through school locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
@@ -239,11 +239,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
--- resume/js/resumeBuilder.js
+++ resume/js/resumeBuilder.js
@@ -125,3 +125,5 @@ var education = {
 	  }
 	]
 }
+
+$("#mapDiv").append(googleMap);
